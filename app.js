f39bb9df1bc9412b0b7a0c1d9ddc923701624b05
amit-gaikwad <amitgaikwad795@gmail.com>
2022-06-20 17:59:00 +0530

    add body parser to read the body data from the req of express app

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded())
 
 app.use('/add-product', (req, res, next) => {
   console.log("this is add product route");
-  res.send("<h2>This is Add Product page</h2>")
-})
+  res.send('<form action="/product" method="POST" > <input name="title"/><button type="submit">Submit</button></form>')
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/")
+});
 
 app.use('/', (req, res, next) => {
   console.log("In The Middleware 2");
