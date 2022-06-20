1cce12870b0e320a403924c07e2d2aebaf691670
amit-gaikwad <amitgaikwad795@gmail.com>
2022-06-20 19:36:04 +0530

    filter routes using prefix string

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 
+//router.get('/admin/add-product', (req, res, next) => {
+
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST" > <input name="title"/><button type="submit">Submit</button></form>')
+  res.send('<form action="/admin/add-product" method="POST" > <input name="title"/><button type="submit">Submit</button></form>')
 });
 
-router.post('/product', (req, res, next) => {
+// router.post('/admin/add-product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect("/")
 });
