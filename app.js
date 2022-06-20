3f38b3803ea329bb55e3472b4c6a7db40df1a6a8
amit-gaikwad <amitgaikwad795@gmail.com>
2022-06-20 19:29:04 +0530

    use express router to create separate route file

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shops');
+
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/add-product', (req, res, next) => {
-  console.log("this is add product route");
-  res.send('<form action="/product" method="POST" > <input name="title"/><button type="submit">Submit</button></form>')
-});
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/")
-});
-
-app.use('/', (req, res, next) => {
-  console.log("In The Middleware 2");
-  res.send("<h1>This is from Express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(8080);
