9ac9e6411e4a3c200d0e13aa79fcb0620885f77a
amit-gaikwad <amitgaikwad795@gmail.com>
2022-06-20 17:46:01 +0530

    add sample route to understand the thingsusing middleware

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In The Middleware 1");
-  next();
+app.use('/add-product', (req, res, next) => {
+  console.log("this is add product route");
+  res.send("<h2>This is Add Product page</h2>")
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log("In The Middleware 2");
-  res.send("<h1>This is from express</h1>");
-})
+  res.send("<h1>This is from Express</h1>");
+});
 
 app.listen(8080);
