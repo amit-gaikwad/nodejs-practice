c8105cc20b42de41ddc2e1d4c894c93c4118f836
Amit Gaikwad <amit.gaikwad@apptware.com>
2022-06-20 09:48:35 +0530

    study about the req

--- app.js
+++ app.js
@@ -15,10 +15,26 @@ const http = require("http");
 
 // event driver architecture for callback and promise function
 const server = http.createServer((req, res) => {
- // console.log("req>>>", req);
+  // console.log("req>>>", req);
+  //exit server
+  // process.exit()
+  //console.log(req.url, req.method, req.headers);
 
 }) // create a server 
 
 // on going listening
 
 server.listen(8080);
+
+
+
+/**
+ * study request object
+ * heders -> 
+ * host
+ * cache control
+ * req.url
+ * req.method
+ * req.headers
+ * 
+ */
