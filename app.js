2aa7f069149f5a81bee1e9debe16ca98fd8ab873
amit-gaikwad <amitgaikwad795@gmail.com>
2022-06-20 17:01:28 +0530

    know about express js and install it

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-/**
- * http, https, fs, path and  os are default module
- */
 
-/**
- * http -> launch a server and send requests
- * https -> launch a SSL server
- * fs -> 
- * require -> import files into any file it is default method exposed by nodejs
- * 
- */
 
 const http = require("http");
-const routes = require('./routes');
 
-// event driver architecture for callback and promise function
-const server = http.createServer(routes.handler) // create a server 
+const express = require("express");
+
+const app = express();
+
+
+const server = http.createServer(app) // create a server 
 
 server.listen(8080);
